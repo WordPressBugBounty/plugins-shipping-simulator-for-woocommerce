2f0790f44e956df4ec8abdebc18716950c8ae4e4
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 05:12:58 +0000

    2.3.4

---
+++ shipping-simulator-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '66288fcf6edfd33510958039ee6311a978667ad0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '66288fcf6edfd33510958039ee6311a978667ad0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
